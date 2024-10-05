c259eef6d94c24595fb623b710dfa8bdc2f6be22
aisfak07 <isfakahmed163@gmail.com>
2024-10-05 17:02:53 +0300

    Product details show from backend Done!

--- backend/djangobnb_backend/property/admin.py
+++ backend/djangobnb_backend/property/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin #type: ignore
-from .models import Property
+from .models import Property, Reservation
 
 # Register your models here.
 admin.site.register(Property)
+admin.site.register(Reservation)
